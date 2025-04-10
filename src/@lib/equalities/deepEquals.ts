667b26bc062ab90ab2a24dad98edcd732f57cd56
tgyeom <ktg5679@gmail.com>
2025-04-10 19:14:19 +0900

    ✅ 비교함수 구현 테스트 통과

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,21 @@
+const isObject = (obj: unknown): obj is Record<string, unknown> => {
+  return obj !== null && typeof obj === "object";
+};
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+    return objA.every((value, index) => deepEquals(objB[index], value));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    if (entriesA.length !== entriesB.length) return false;
+    return entriesA.every(([key, value]) => deepEquals(objB[key], value));
+  }
+
   return objA === objB;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,30 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+
+  if (objA === objB) {
+    return true;
+  }
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (
+      !objB.hasOwnProperty(key) ||
+      (objA as any)[key] !== (objB as any)[key]
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
