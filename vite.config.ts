c60c6dad482c51e903947bd7350507c5b4cdce46
tgyeom <ktg5679@gmail.com>
2025-04-11 00:15:15 +0900

    ✅ gh-pages 배포

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
+export default ({ mode }: UserConfig) =>
+  mergeConfig(
+    defineConfig({
+      plugins: [react()],
+      base: mode === "production" ? "/front_5th_chapter1-3/" : "/",
+    }),
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+          reportsDirectory: "./.coverage",
+          reporter: ["lcov", "json", "json-summary"],
+        },
       },
-    },
-  }),
-);
+    })
+  );
